d653bd96170370a866e9cb7ea64125d7461bf609
Roberto Rivera <rzrivera@protonmail.com>
2021-03-17 01:42:13 -0700

    adjust for partition check  function media_part

--- lib/functions.sh
+++ lib/functions.sh
@@ -127,8 +127,9 @@ get_pass() {
 
 open_luks() {
 	log "Unlock luks volume"
+	media_part
 	if [ "$(sudo blkid | grep -c /dev/mapper/crypt_root)" == 0 ]; then
-		if ! suds "echo -n ${1//$/\\$} | cryptsetup --key-file=- luksOpen ${cfg_scadrial_device_name}2 ${cfg_scadrial_device_luks}"; then
+		if ! suds "echo -n ${1//$/\\$} | cryptsetup --key-file=- luksOpen ${cfg_scadrial_device_name}${ppart}2 ${cfg_scadrial_device_luks}"; then
 			err "Luks Unlock Failed"
 		else
 			echo "Luks Unlocked"
@@ -136,27 +137,35 @@ open_luks() {
 	fi 
 }
 
+media_part() {
+	if [ "$(< /proc/partitions grep "${cfg_scadrial_device_name}" | awk '{print $4}' | grep -c p)" == "0" ]; then
+		ppart=""
+	else
+		ppart="p"
+	fi
+}
+
 media_mount() {
 	get_pass
 	open_luks "$passphrase"
 
-    log "Mount the partitions"
-    #----------------------------------------------------------------------------
-    suds "rm -rf ${cfg_scadrial_chroot_path}"
-    suds "mkdir -p ${cfg_scadrial_chroot_path}"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@      /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}"
-    suds "mkdir -p ${cfg_scadrial_chroot_path}/boot"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@boot  /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/boot"
-    suds "mkdir -p ${cfg_scadrial_chroot_path}/{boot/efi,home,opt/mistborn_volumes,var}"
-    suds "mount ${cfg_scadrial_device_name}1 ${cfg_scadrial_chroot_path}/boot/efi"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@home  /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/home"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@data  /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/opt/mistborn_volumes"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@var   /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var"
-    suds "mkdir -p ${cfg_scadrial_chroot_path}/{var/log,var/tmp}"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@log   /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var/log"
-    suds "mkdir -p ${cfg_scadrial_chroot_path}/var/log/audit"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@audit /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var/log/audit"
-    suds "mount -o ${cfg_scadrial_device_optn},subvol=@tmp   /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var/tmp"
+	log "Mount the partitions"
+	#----------------------------------------------------------------------------
+	suds "rm -rf ${cfg_scadrial_chroot_path}"
+	suds "mkdir -p ${cfg_scadrial_chroot_path}"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@      /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}"
+	suds "mkdir -p ${cfg_scadrial_chroot_path}/boot"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@boot  /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/boot"
+	suds "mkdir -p ${cfg_scadrial_chroot_path}/{boot/efi,home,opt/mistborn_volumes,var}"
+	suds "mount ${cfg_scadrial_device_name}${ppart}1 ${cfg_scadrial_chroot_path}/boot/efi"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@home  /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/home"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@data  /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/opt/mistborn_volumes"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@var   /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var"
+	suds "mkdir -p ${cfg_scadrial_chroot_path}/{var/log,var/tmp}"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@log   /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var/log"
+	suds "mkdir -p ${cfg_scadrial_chroot_path}/var/log/audit"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@audit /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var/log/audit"
+	suds "mount -o ${cfg_scadrial_device_optn},subvol=@tmp   /dev/mapper/crypt_root ${cfg_scadrial_chroot_path}/var/tmp"
 }
 
 media_reset() {
@@ -184,62 +193,64 @@ media_reset() {
 }
 
 media_setup() {
-    #----------------------------------------------------------------------------
+	#----------------------------------------------------------------------------
 	# Function configure the specified media per the config.yaml
 	# Includes partitioning, encryption, mounting, etc.
-    #----------------------------------------------------------------------------
+	#----------------------------------------------------------------------------
 	get_pass
 
-    #----------------------------------------------------------------------------
-    log "Setup storage media"
-    #----------------------------------------------------------------------------
+	#----------------------------------------------------------------------------
+	log "Setup storage media"
+	#----------------------------------------------------------------------------
 
 	media_reset
 
-    suds "sgdisk -og ${cfg_scadrial_device_name} \
-    --new=1::+260M  --typecode=1:EF00 --change-name=1:'EFI partition' \
-    --new=2::0      --typecode=2:8304 --change-name=2:'Linux partition'"
+	suds "sgdisk -og ${cfg_scadrial_device_name} \
+	--new=1::+260M  --typecode=1:EF00 --change-name=1:'EFI partition' \
+	--new=2::0      --typecode=2:8304 --change-name=2:'Linux partition'"
 
-    suds "sgdisk -p ${cfg_scadrial_device_name}"
-    suds "partprobe ${cfg_scadrial_device_name}"
+	suds "sgdisk -p ${cfg_scadrial_device_name}"
+	suds "partprobe ${cfg_scadrial_device_name}"
 
-    suds "wipefs -af ${cfg_scadrial_device_name}1"
-    suds "wipefs -af ${cfg_scadrial_device_name}2"
+	media_part
 
-    #----------------------------------------------------------------------------
-    log "Setup encryption"
+	suds "wipefs -af ${cfg_scadrial_device_name}${ppart}1"
+	suds "wipefs -af ${cfg_scadrial_device_name}${ppart}2"
+
+	#----------------------------------------------------------------------------
+	log "Setup encryption"
 	# Escape any dollar signs in the password
-    suds "echo -n ${passphrase//$/\\$} | cryptsetup -q -v --iter-time 5000 --type luks2 \
-        --hash sha512 --use-random luksFormat ${cfg_scadrial_device_name}2 -"
+	suds "echo -n ${passphrase//$/\\$} | cryptsetup -q -v --iter-time 5000 --type luks2 \
+	    --hash sha512 --use-random luksFormat ${cfg_scadrial_device_name}${ppart}2 -"
 	
 	# shellcheck disable=SC2086
-    open_luks $passphrase
+	open_luks $passphrase
 
-    #----------------------------------------------------------------------------
-    log "Create filesystems"
-    suds "mkfs.vfat -vF32 ${cfg_scadrial_device_name}1"
-    suds "mkfs.btrfs -L crypt_root /dev/mapper/${cfg_scadrial_device_luks}"
+	#----------------------------------------------------------------------------
+	log "Create filesystems"
+	suds "mkfs.vfat -vF32 ${cfg_scadrial_device_name}${ppart}1"
+	suds "mkfs.btrfs -L crypt_root /dev/mapper/${cfg_scadrial_device_luks}"
 
-    suds "rm -rf ${cfg_scadrial_device_pool} && mkdir ${cfg_scadrial_device_pool}"
-    suds "mount -t btrfs -o ${cfg_scadrial_device_optn} /dev/mapper/crypt_root ${cfg_scadrial_device_pool}"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@boot"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@home"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@data"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@var"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@log"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@audit"
-    suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@tmp"
+	suds "rm -rf ${cfg_scadrial_device_pool} && mkdir ${cfg_scadrial_device_pool}"
+	suds "mount -t btrfs -o ${cfg_scadrial_device_optn} /dev/mapper/crypt_root ${cfg_scadrial_device_pool}"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@boot"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@home"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@data"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@var"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@log"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@audit"
+	suds "btrfs subvolume create ${cfg_scadrial_device_pool}/@tmp"
 
-    suds "umount ${cfg_scadrial_device_pool}"
+	suds "umount ${cfg_scadrial_device_pool}"
 
 	media_mount
 
-    #----------------------------------------------------------------------------
-    log "Bootstrap the new system"
-    #----------------------------------------------------------------------------
-    suds "debootstrap --arch amd64 $cfg_scadrial_dist_name $cfg_scadrial_chroot_path" http://archive.ubuntu.com/ubuntu
-    for b in dev dev/pts proc sys; do suds "mount -B /$b $cfg_scadrial_chroot_path/$b"; done
+	#----------------------------------------------------------------------------
+	log "Bootstrap the new system"
+	#----------------------------------------------------------------------------
+	suds "debootstrap --arch amd64 $cfg_scadrial_dist_name $cfg_scadrial_chroot_path" http://archive.ubuntu.com/ubuntu
+	for b in dev dev/pts proc sys; do suds "mount -B /$b $cfg_scadrial_chroot_path/$b"; done
 }
 
 script_setup() {
@@ -281,14 +292,15 @@ script_setup() {
 	#----------------------------------------------------------------------------
 	log "Configure partitions"
 	#----------------------------------------------------------------------------
-	pluks="$(blkid -s PARTUUID -o value ${cfg_scadrial_device_name}2)"
+	media_part
+	pluks="$(blkid -s PARTUUID -o value ${cfg_scadrial_device_name}${ppart}2)"
 
 	# Setup the device UUID that contains our luks volume
 	echo "# <target name>	<source device>		<key file>	<options>" > /etc/crypttab
 	echo "$cfg_scadrial_device_luks PARTUUID=$pluks none luks,discard" >> /etc/crypttab
 
 	# Setup the partitions
-	eboot="$(blkid -s UUID -o value ${cfg_scadrial_device_name}1)"
+	eboot="$(blkid -s UUID -o value ${cfg_scadrial_device_name}${ppart}1)"
 	croot="$(blkid -s UUID -o value /dev/mapper/$cfg_scadrial_device_luks)"
 
 	cat <<- EOF | tee /etc/fstab
@@ -385,10 +397,7 @@ script_setup() {
 
 	#----------------------------------------------------------------------------
 	figlet "Scadrial: Wrap-up"
-	log "The initial media configuration complete. Pending steps to complete on the host."
-	echo "Exit chroot and umount our media, as follows:"
 	#----------------------------------------------------------------------------
-	
 	# Enable wan interface to identify IP address
 	cat <<- EOF > /etc/netplan/01-netcfg.yaml
 	# This file describes the network interfaces available on your system
@@ -402,6 +411,9 @@ script_setup() {
 	EOF
 	
 	netplan apply
+
+	log "The initial media configuration complete. Pending steps to complete on the host."
+	echo "Exit chroot and umount our media, as follows:"
 	echo "exit"
 	echo "sudo ./scadrial-setup.sh unmount"
 
